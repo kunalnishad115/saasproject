77ef65ae0b10eee1eabc8a99bd5cfad9c68af2bc
kunalnishad115 <nishadkunal1234567@gmail.com>
2025-06-10 00:12:41 +0530

    adding the cta and List ui implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,57 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
+import Card from '@/components/companionCard'
+import CompanionList from '@/components/campanionList'
+import Cta from '@/components/cta'
+import { recentSessions } from '@/constants'
+
+
+
 const Page = () => {
   return (
-    <div>Page
+    <main>
       <h1 className='text-2xl underline'> 
-        SAAS
-        <Button>
-          Click Me
-        </Button>
+        Popular Companoins
+       
       </h1>
-    </div>
+
+      <section className='home-section'>
+      < Card id="1"
+      name="Neural Network"
+      topic="Machine Learning"
+      subject="AI"
+      duration={30}
+      color="#ffda6e"
+      
+       />
+     < Card 
+     id="2"
+      name="Number Theory"
+      topic="Mathematics"
+      subject="Math"
+      duration={30}
+      color="#e5d0ff"/>
+     < Card 
+     id="3"
+      name="vocabulary"
+      topic="Language Learning"
+      subject="English"
+      duration={30}
+      color="#BDE7FF"/>
+
+       </section>
+
+
+       <section className='home-section'>
+        <CompanionList
+        title="Recently Completed Section"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg-full"
+        />
+        <Cta/>
+     
+       </section>
+    </main>
   )
 }
 
